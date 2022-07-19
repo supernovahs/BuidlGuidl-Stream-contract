81ef3e5bed6e5d959adc1a629fd9e512de3813f5
supernovahs <harshitsinghal252@gmail.com>
2022-07-19 19:37:40 +0530

    Chore: Cleanup

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -64,56 +64,5 @@ contract ContractTest is Test {
 
 
 
-//     function testOptimized() public {
-//         assertTrue(true);
-//         uint a = streamcontract.streamBalance();
-//         uint64 b = streamcontract.last();
-//         uint c = streamcontract.cap();
-//         console.log(
-//             "last time",b
-//         );
-//         console.log("cap",c);
-//         console.log("a",a);
-//         // assertTrue(a == 0.5 ether);
-//     }
-
-//     function testOriginal() public {
-//         assertTrue(true);
-//         uint a = simplecontract.streamBalance();
-//         uint b = simplecontract.last();
-//         uint c = simplecontract.cap();
-//         console.log(
-//             "last time",b
-//         );
-//         console.log("cap",c);
-//         console.log("a",a);
-//         // assertTrue(a == 0.5 ether);
-
-//     }
-
-//    function testdeposit() public {
-//        streamcontract.streamDeposit{value: 10000000000000000}("hello");
-//        uint bal;
-//        address stream= address(streamcontract);
-//        assembly{
-//            bal:= balance(stream)
-//        }
-//        assertTrue(bal == 10000000000000000);
-//    }
-
-//    function testwithdraw() public {
-      
-//        vm.deal(0x1b37B1EC6B7faaCbB9AddCCA4043824F36Fb88D8,10 ether);
-//         streamcontract.streamDeposit{value: 10000000000000000}("hello");
-//        vm.prank(0x1b37B1EC6B7faaCbB9AddCCA4043824F36Fb88D8);
-//      streamcontract.streamWithdraw(10000000,"Buidling");
-    
-//    }
-
-//    function testwithdraworiginal() public {
-//        vm.deal(0x1b37B1EC6B7faaCbB9AddCCA4043824F36Fb88D8,10 ether);
-//         simplecontract.streamDeposit{value: 10000000000000000}("hello");
-//        vm.prank(0x1b37B1EC6B7faaCbB9AddCCA4043824F36Fb88D8);
-//      simplecontract.streamWithdraw(10000000,"Buidling");
-//    }
+//  
 }
